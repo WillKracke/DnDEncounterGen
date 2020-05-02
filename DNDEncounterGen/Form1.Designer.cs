6a5aafe4190f62b7bd3e66f2397724d2d175e6c6
WillKracke <wjk4218@g.rit.edu>
2020-05-02 03:52:44 -0400

    Fixed radiobuttons updating, added all difficulty arrays

--- DNDEncounterGen/Form1.Designer.cs
+++ DNDEncounterGen/Form1.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.grpCR = new System.Windows.Forms.GroupBox();
-            this.txtPlayerCt = new System.Windows.Forms.TextBox();
-            this.txtPlayerLvl = new System.Windows.Forms.TextBox();
-            this.txtCR = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.rbDead = new System.Windows.Forms.RadioButton();
+            this.rbHard = new System.Windows.Forms.RadioButton();
+            this.rbMed = new System.Windows.Forms.RadioButton();
+            this.rbEasy = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCR = new System.Windows.Forms.TextBox();
+            this.txtPlayerLvl = new System.Windows.Forms.TextBox();
+            this.txtPlayerCt = new System.Windows.Forms.TextBox();
             this.grpCR.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -62,55 +62,64 @@
             this.grpCR.TabStop = false;
             this.grpCR.Text = "Combat Rating";
             // 
-            // txtPlayerCt
+            // groupBox1
             // 
-            this.txtPlayerCt.Location = new System.Drawing.Point(6, 35);
-            this.txtPlayerCt.Name = "txtPlayerCt";
-            this.txtPlayerCt.Size = new System.Drawing.Size(43, 20);
-            this.txtPlayerCt.TabIndex = 0;
-            this.txtPlayerCt.TextChanged += new System.EventHandler(this.txtPlayerCt_TextChanged);
+            this.groupBox1.Controls.Add(this.rbDead);
+            this.groupBox1.Controls.Add(this.rbHard);
+            this.groupBox1.Controls.Add(this.rbMed);
+            this.groupBox1.Controls.Add(this.rbEasy);
+            this.groupBox1.Location = new System.Drawing.Point(8, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 68);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Threshold Level";
             // 
-            // txtPlayerLvl
+            // rbDead
             // 
-            this.txtPlayerLvl.Location = new System.Drawing.Point(67, 35);
-            this.txtPlayerLvl.Name = "txtPlayerLvl";
-            this.txtPlayerLvl.Size = new System.Drawing.Size(43, 20);
-            this.txtPlayerLvl.TabIndex = 1;
-            this.txtPlayerLvl.TextChanged += new System.EventHandler(this.txtPlayerLvl_TextChanged);
+            this.rbDead.AutoSize = true;
+            this.rbDead.Location = new System.Drawing.Point(111, 43);
+            this.rbDead.Name = "rbDead";
+            this.rbDead.Size = new System.Drawing.Size(58, 17);
+            this.rbDead.TabIndex = 3;
+            this.rbDead.Text = "Deadly";
+            this.rbDead.UseVisualStyleBackColor = true;
+            this.rbDead.CheckedChanged += calcCombatRating;
             // 
-            // txtCR
+            // rbHard
             // 
-            this.txtCR.Location = new System.Drawing.Point(141, 35);
-            this.txtCR.Name = "txtCR";
-            this.txtCR.Size = new System.Drawing.Size(95, 20);
-            this.txtCR.TabIndex = 2;
+            this.rbHard.AutoSize = true;
+            this.rbHard.Location = new System.Drawing.Point(111, 20);
+            this.rbHard.Name = "rbHard";
+            this.rbHard.Size = new System.Drawing.Size(80, 17);
+            this.rbHard.TabIndex = 2;
+            this.rbHard.Text = "Challenging";
+            this.rbHard.UseVisualStyleBackColor = true;
+            this.rbHard.CheckedChanged += calcCombatRating;
             // 
-            // label1
+            // rbMed
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 58);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 26);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Num of \r\nPlayers";
+            this.rbMed.AutoSize = true;
+            this.rbMed.Location = new System.Drawing.Point(7, 43);
+            this.rbMed.Name = "rbMed";
+            this.rbMed.Size = new System.Drawing.Size(70, 17);
+            this.rbMed.TabIndex = 1;
+            this.rbMed.Text = "Moderate";
+            this.rbMed.UseVisualStyleBackColor = true;
+            this.rbMed.CheckedChanged += calcCombatRating;
             // 
-            // label2
+            // rbEasy
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(64, 58);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 26);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Player \r\nLevel";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(116, 38);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(19, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "-->";
+            this.rbEasy.AutoSize = true;
+            this.rbEasy.Checked = true;
+            this.rbEasy.Location = new System.Drawing.Point(7, 20);
+            this.rbEasy.Name = "rbEasy";
+            this.rbEasy.Size = new System.Drawing.Size(48, 17);
+            this.rbEasy.TabIndex = 0;
+            this.rbEasy.TabStop = true;
+            this.rbEasy.Text = "Easy";
+            this.rbEasy.UseVisualStyleBackColor = true;
+            this.rbEasy.CheckedChanged += calcCombatRating;
             // 
             // label4
             // 
@@ -121,60 +130,55 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Avg Combat Rating";
             // 
-            // groupBox1
+            // label3
             // 
-            this.groupBox1.Controls.Add(this.radioButton4);
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(8, 88);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(232, 68);
-            this.groupBox1.TabIndex = 7;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Threshold Level";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "-->";
             // 
-            // radioButton1
+            // label2
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(7, 20);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(48, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Easy";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Player \r\nLevel";
             // 
-            // radioButton2
+            // label1
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(7, 43);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(70, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.Text = "Moderate";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Num of \r\nPlayers";
             // 
-            // radioButton3
+            // txtCR
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(111, 20);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(80, 17);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.Text = "Challenging";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.txtCR.Location = new System.Drawing.Point(141, 35);
+            this.txtCR.Name = "txtCR";
+            this.txtCR.Size = new System.Drawing.Size(95, 20);
+            this.txtCR.TabIndex = 2;
             // 
-            // radioButton4
+            // txtPlayerLvl
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(111, 43);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(58, 17);
-            this.radioButton4.TabIndex = 3;
-            this.radioButton4.Text = "Deadly";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.txtPlayerLvl.Location = new System.Drawing.Point(67, 35);
+            this.txtPlayerLvl.Name = "txtPlayerLvl";
+            this.txtPlayerLvl.Size = new System.Drawing.Size(43, 20);
+            this.txtPlayerLvl.TabIndex = 1;
+            this.txtPlayerLvl.TextChanged += calcCombatRating;
+            // 
+            // txtPlayerCt
+            // 
+            this.txtPlayerCt.Location = new System.Drawing.Point(6, 35);
+            this.txtPlayerCt.Name = "txtPlayerCt";
+            this.txtPlayerCt.Size = new System.Drawing.Size(43, 20);
+            this.txtPlayerCt.TabIndex = 0;
+            this.txtPlayerCt.TextChanged += calcCombatRating;
             // 
             // Form1
             // 
@@ -203,10 +207,10 @@
         private System.Windows.Forms.TextBox txtPlayerLvl;
         private System.Windows.Forms.TextBox txtPlayerCt;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rbDead;
+        private System.Windows.Forms.RadioButton rbHard;
+        private System.Windows.Forms.RadioButton rbMed;
+        private System.Windows.Forms.RadioButton rbEasy;
     }
 }
 
--- DNDEncounterGen/Form1.cs
+++ DNDEncounterGen/Form1.cs
@@ -19,20 +19,12 @@ namespace DNDEncounterGen
 
         // Create arrays with XP threshold by character level
         int[] easyThreshold = { 50, 100, 200, 350, 550, 800, 1100, 1500, 1700, 2200, 2800, 3500, 4400, 5400, 6500, 7700, 9000, 10400, 11900, 13500 };
-
-        // When either textbox is changed for calculating CR, trigger the calculation method
-        private void txtPlayerCt_TextChanged(object sender, EventArgs e)
-        {
-            calcCombatRating();
-        }
-
-        private void txtPlayerLvl_TextChanged(object sender, EventArgs e)
-        {
-            calcCombatRating();
-        }
+        int[] mediumThreshold = { 75, 150, 300, 525, 825, 1299, 1650, 2250, 2550, 3300, 4200, 5250, 6600, 8100, 9750, 11550, 13500, 15600, 17850, 20250 };
+        int[] hardThreshold = { 100, 200, 400, 700, 1100, 1600, 2200, 3000, 3400, 4400, 5600, 7000, 8800, 10800, 13000, 15400, 18000, 20800, 23800, 27000 };
+        int[] deadlyThreshold = { 125, 250, 500, 875, 1375, 2000, 2750, 3750, 4250, 5500, 7000, 8750, 11000, 13500, 16250, 19250, 22500, 26000, 29750, 33750 };
         
         // Calculates CR, after checking if both textboxes are full. Requires num of players and level of players
-        private void calcCombatRating()
+        private void calcCombatRating(object sender, EventArgs e)
         {
             int numPlayers = 0;
             int playerLvl = 0;
@@ -46,11 +38,20 @@ namespace DNDEncounterGen
             {
                 try
                 {
+                    // Get the single PC CR from the appropriate array
+                    int playerCR = 0;
+
+                    if (rbEasy.Checked) { playerCR = easyThreshold[playerLvl - 1]; }
+                    if (rbMed.Checked) { playerCR = mediumThreshold[playerLvl - 1]; }
+                    if (rbHard.Checked) { playerCR = hardThreshold[playerLvl - 1]; }
+                    if (rbDead.Checked) { playerCR = deadlyThreshold[playerLvl - 1]; }
+
                     // Calc CR, move it to output box
-                    int avgCR = easyThreshold[playerLvl - 1] * numPlayers;
+                    int avgCR = playerCR * numPlayers;
 
                     txtCR.Text = avgCR.ToString();
                 }
+                // If calculation error, print it to the output instead of crashing
                 catch (Exception)
                 {
                     txtCR.Text = "Error";
